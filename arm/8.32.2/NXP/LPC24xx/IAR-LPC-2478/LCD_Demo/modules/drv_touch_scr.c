04bffb0d2e6bed00665d708e6c3b71d052e40614
s163905 <s163905@student.dtu.dk>
2019-01-09 10:57:24 +0100

    Added a seoerate file to work on DAC

--- arm/8.32.2/NXP/LPC24xx/IAR-LPC-2478/LCD_Demo/modules/drv_touch_scr.c
+++ arm/8.32.2/NXP/LPC24xx/IAR-LPC-2478/LCD_Demo/modules/drv_touch_scr.c
@@ -347,17 +347,17 @@ void TouchScrInit (void)
 
   // Init delay timer
   PCONP_bit.PCTIM1 = 1; // Enable TIM0 clocks
-  T1TCR = 2;            // stop and reset timer 1
-  T1CTCR_bit.CTM = 0;   // Timer Mode: every rising PCLK edge
-  T1MCR_bit.MR0S = 1;   // stop timer if MR0 matches the TC
-  T1MCR_bit.MR0R = 1;   // enable timer reset if MR0 matches the TC
-  T1MCR_bit.MR0I = 1;   // Enable Interrupt on MR0
-  T1PR = (SYS_GetFpclk(TIMER1_PCLK_OFFSET)/ 1000000) - 1; // 1us resolution
-  T1MR0 = TS_SETUP_DLY;
-  T1IR_bit.MR0INT = 1;  // clear pending interrupt
+  T0TCR = 2;            // stop and reset timer 0
+  T0CTCR_bit.CTM = 0;   // Timer Mode: every rising PCLK edge
+  T0MCR_bit.MR0S = 1;   // stop timer if MR0 matches the TC
+  T0MCR_bit.MR0R = 1;   // enable timer reset if MR0 matches the TC
+  T0MCR_bit.MR0I = 1;   // Enable Interrupt on MR0
+  T0PR = (SYS_GetFpclk(TIMER1_PCLK_OFFSET)/ 1000000) - 1; // 1us resolution
+  T0MR0 = TS_SETUP_DLY;
+  T0IR_bit.MR0INT = 1;  // clear pending interrupt
   VIC_SetVectoredIRQ(TimerIntr_Handler,TS_INTR_PRIORITY,VIC_TIMER0);
-  VICINTENABLE |= 1UL << VIC_TIMER1;
-  T1TCR = 1;            // start timer 1
+  VICINTENABLE |= 1UL << VIC_TIMER0;
+  T0TCR = 1;            // start timer 0
 }
 
 /*************************************************************************

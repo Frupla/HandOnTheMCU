5740fab379ce2b5cc7c1bc40aca3e933c3dff873
s163905 <s163905@student.dtu.dk>
2019-01-11 11:55:28 +0100

    Implemented current readings and re-implented frequency calculations based on several periods

--- arm/8.32.2/NXP/LPC24xx/IAR-LPC-2478/evseProject/app/main.c
+++ arm/8.32.2/NXP/LPC24xx/IAR-LPC-2478/evseProject/app/main.c
@@ -64,18 +64,30 @@ extern FontType_t Terminal_18_24_12;
 #define P11_MASK (1UL<<11)
 
 Int32U timetick = 0;
-Int32U x = 0;
-Int32U y_old = 0;
+Int32U x2 = 0;
+Int32U x3 = 0;
+Int32U y2_old = 0;
+Int32U y3_old = 0;
 Int32U t_old = 0;
-Int32U x_old = 0;
-float crosstick = 0;
-float crosstick_old = 0;
+Int32U x2_old = 0;
+Int32U x3_old = 0;
+int i2 = 0;
+int i3 = 0;
+#define N_O_PERIODS 16
+#define channel3 false
+#define channel2 true
+float crosstick2 = 0;
+float crosstick2_old = 0;
+float crosstick3 = 0;
+float crosstick3_old = 0;
 float alpha =0;
-float T = 0;
-float f = 0;
+float T2 = 0;
+float T3 = 0;
+//float f = 0;
 Boolean VrefInTheLastCycle = false;
 Boolean tickCrossingZero = false;
-Boolean waitingForCross = true;
+Boolean waitingForCross2 = true;
+Boolean waitingForCross3 = true;
 Boolean timeToPrint = true;
 
 
@@ -88,10 +100,15 @@ Boolean timeToPrint = true;
  * Description: low passes the signal
  *
  *************************************************************************/
-Int32U lowPass(Int32U x){
-  Int32U y   = (Int32U)(alpha*x + (1 - alpha)*y_old);
-
-  y_old = y;
+Int32U lowPass(Int32U x, Boolean channel){
+  Int32U y = 0;
+  if (channel){
+    y   = (Int32U)(alpha*x + (1 - alpha)*y2_old);
+    y2_old = y;
+  } else{
+    y   = (Int32U)(alpha*x + (1 - alpha)*y3_old);
+    y3_old = y;
+  }
   return y;
 }
 /*************************************************************************
@@ -117,9 +134,14 @@ void TIMER1IntrHandler (void)
   }
   
   if(ADDR2_bit.DONE){
-    x_old = x;
-    x = lowPass(ADDR2_bit.RESULT);
-    DACR_bit.VALUE = x;
+    x2_old = x2;
+    x2 = lowPass(ADDR2_bit.RESULT,channel2);
+    //DACR_bit.VALUE = x;
+  }
+  
+  if(ADDR3_bit.DONE){
+    x3_old = x3;
+    x3 = lowPass(ADDR3_bit.RESULT,channel3);
   }
   
 
@@ -139,20 +161,43 @@ void TIMER1IntrHandler (void)
     VrefInTheLastCycle = false;
   }
   */
-  if (waitingForCross && x >= 512){
-    crosstick = timetick - (float)x/((float)x-(float)x_old);
-    if (tickCrossingZero){
-      T = crosstick+5000 - crosstick_old;
-      tickCrossingZero = false;
-    } else{
-      T = crosstick - crosstick_old;
+  // Channel 2, aka voltage measurements
+  if (waitingForCross2 && x2 >= 512){
+    if (i2 >= N_O_PERIODS){
+      crosstick2 = timetick - (float)x2/((float)x2-(float)x2_old);
+      if (tickCrossingZero){
+        T2 = crosstick2+5000 - crosstick2_old;
+        tickCrossingZero = false;
+      } else{
+        T2 = crosstick2 - crosstick2_old;
+      }
+      crosstick2_old = crosstick2;
+      i2 = 0;
     }
-    crosstick_old = crosstick;
-    waitingForCross = false;
-  } else if(!waitingForCross && x <= 512){
-    waitingForCross = true;
+    waitingForCross2 = false;
+    i2++;
+  } else if(!waitingForCross2 && x2 <= 512){
+    waitingForCross2 = true;
   }
+ 
   
+  //Channel3, aka current measurements
+  if (waitingForCross3 && x3 >= 512){
+    if (i3 <= N_O_PERIODS) {
+      crosstick3 = timetick - (float)x3/((float)x3-(float)x3_old);
+      if (tickCrossingZero){
+        T3 = crosstick3+5000 - crosstick3_old;
+        tickCrossingZero = false;
+      } else{
+        T3 = crosstick3 - crosstick3_old;
+      }
+      crosstick3_old = crosstick3;
+    }
+    waitingForCross3 = false;
+    i3++;
+  } else if(!waitingForCross3 && x3 <= 512){
+    waitingForCross3 = true;
+  }
   
   // clear interrupt
   T1IR_bit.MR1INT = 1;
@@ -178,8 +223,8 @@ void ADC_Init (void){
   AD0CR_bit.BURST = 1; //0=ADC is set to operate in software controlled mode, 1= continue mode
   PINSEL1_bit.P0_25 = 0x1; //AD0[2]
   PINMODE1_bit.P0_25 = 0x2;
-  //PINSEL1_bit.P0_26 = 0x1; //AD0[3]
-  //PINMODE1_bit.P0_26 = 0x2;
+  PINSEL1_bit.P0_26 = 0x1; //AD0[3]
+  PINMODE1_bit.P0_26 = 0x2;
   ADINTEN_bit.ADGINTEN = 0; //When 0, only the individual A/D channels enabled by ADINTEN 7:0 will generate interrupts.
   ADINTEN_bit.ADINTEN0 = 1; //Enable interrupt
   ADINTEN_bit.ADINTEN1 = 1;
@@ -188,7 +233,7 @@ void ADC_Init (void){
 //  AD0CR_bit.START = 1;
 //  VIC_SetVectoredIRQ (TIMER1IntrHandler,2,VIC_TIMER1);
 //  VICINTENABLE |= 1UL << VIC_TIMER1;
-  AD0CR_bit.SEL = 0x04; // Channel 0, 1, 2 and 3 enabled. 1111
+  AD0CR_bit.SEL = 0x06; // Channel 0, 1, 2 and 3 enabled: 1111, Channel 2 and 3 enabled:0110
   AD0CR_bit.PDN = 1; //The A/D Converter is operational
 }
 
@@ -270,12 +315,13 @@ int main(void)
   GLCD_Ctrl (TRUE);
   
 
-  //initialize DAC
+  //initialize DAC - commented out as we need the pin for current sampling
+  /*
   PINSEL1_bit.P0_26=2; //sets pin function to AOUT
   DACR_bit.BIAS=1; //set BIAS mode 1
   PCLKSEL0_bit.PCLK_DAC=1; //enable clock signal
   DACR_bit.VALUE = 0X3FF;
-
+  */
    GLCD_SetFont(&Terminal_18_24_12,0x00ffffff,0x0000000);
    GLCD_SetWindow(95,10,265,33);
    GLCD_TextSetPos(0,0);
@@ -285,7 +331,8 @@ int main(void)
    Int32U fc = 50; //Value of cut off frequency
    float RC = (1/(2*3.1415*fc));
    alpha = (1/(float)TIMER1_TICK_PER_SEC)/(RC+(1/(float)TIMER1_TICK_PER_SEC));
-   float F = 0;
+   float F2 = 0;
+   float F3 = 0;
    
    
    FIO0DIR = P19_MASK | P11_MASK; // Setting pin 19 to be an output
@@ -295,23 +342,39 @@ int main(void)
      
      // Here we handle all the printing that goes of twice a second
      if(timeToPrint){
-        FIO0PIN |= P11_MASK;
-        F = TIMER1_TICK_PER_SEC/T;
+        //FIO0PIN |= P11_MASK;
+        //Calculating and printing voltage frequency
+        F2 = TIMER1_TICK_PER_SEC*N_O_PERIODS/T2;
         char MyString [ 100 ]; // destination string
         int d,f1,f2,f3;
-        d = (int) F; // Decimal precision: 3 digits
-        f1 = (int)(10*(F-(float)d));
-        f2 = (int)(100*(F-(float)d)) - 10*f1;
-        f3 = (int)(1000*(F-(float)d)) - 10*f2 - 100*f1;
+        d = (int) F2; // Decimal precision: 3 digits
+        f1 = (int)(10*(F2-(float)d));
+        f2 = (int)(100*(F2-(float)d)) - 10*f1;
+        f3 = (int)(1000*(F2-(float)d)) - 10*f2 - 100*f1;
         sprintf ( MyString, "Frequency: %d.%d%d%dHz", d, f1,f2,f3); 
           
         GLCD_SetWindow(55,35,279,60);
         GLCD_TextSetPos(0,0);
         GLCD_print(MyString);
-        FIO0PIN &= ~P11_MASK;
+        
+        //Calculating and printing voltage frequency
+        F3 = TIMER1_TICK_PER_SEC*N_O_PERIODS/T3;
+        char MyString3 [ 100 ]; // destination string
+        d = (int) F2; // Decimal precision: 3 digits
+        f1 = (int)(10*(F2-(float)d));
+        f2 = (int)(100*(F2-(float)d)) - 10*f1;
+        f3 = (int)(1000*(F2-(float)d)) - 10*f2 - 100*f1;
+        sprintf ( MyString3, "Frequency: %d.%d%d%dHz", d, f1,f2,f3); 
+          
+        GLCD_SetWindow(55,65,255,90);
+        GLCD_TextSetPos(0,0);
+        GLCD_print(MyString3);
+        
+        //FIO0PIN &= ~P11_MASK;
         timeToPrint = false;
      }   
      // Here we handle the dynamic printing that goes off all the time
+     /*
      if(FIO0PIN & P19_MASK){
           GLCD_SetWindow(55,65,255,90);
           GLCD_TextSetPos(0,0);
@@ -321,9 +384,9 @@ int main(void)
           GLCD_TextSetPos(0,0);
           GLCD_print("Bulb is: OFF");
      }
+     */
      
-     
-     if(F < 48|| F > 52){
+     if(F2 < 48|| F2 > 52){
        FIO0PIN &= ~P19_MASK;
      }else{
        FIO0PIN |= P19_MASK;
